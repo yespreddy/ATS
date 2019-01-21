72cb7ae25adce5cf2f89b87125e0a84adf085793
india <india@DELL>
2019-01-21 15:38:21 +0530

    Country Get Call Added

---
+++ PTG.ATS.Entities/CountryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTG.ATS.Entities
+{
+    public class CountryDTO
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
