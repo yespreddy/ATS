e040df56c68624f53a669d129cf5b9f5729fd301
Bhaskar Ravuri <bhaskar.ravuri@ptgindia.com>
2019-01-31 18:53:21 +0530

    files added

---
+++ PTG.ATS.Services/Controllers/CommonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using PTG.ATS.Entities;
+//using Claims.BE.BO;
+//using Claims.BE.DTO;
+using PTG.ATS.Infra;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PTG.ATS.Services.Controllers
+{
+    //[Authorize]
+    [Produces("application/json")]
+    [Route("api/Common")]
+    public class CommonController : Controller
+    {
+
+        ICommon _common { get; set; }
+
+        public CommonController(ICommon common)
+        {
+            _common = common;
+        }
+
+        //Get Method for Test
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("GetCommon")]
+        public string GetCommon()
+        {
+            return "Common";
+        }
+
+       
+
+    }
+}
---
+++ PTG.ATS.Services/Controllers/JobRequisitionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using PTG.ATS.Entities;
+//using Claims.BE.BO;
+//using Claims.BE.DTO;
+using PTG.ATS.Infra;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PTG.ATS.Services.Controllers
+{
+    //[Authorize]
+    [Produces("application/json")]
+    [Route("api/JobRequisition")]
+    public class JobRequisitionController : Controller
+    {
+
+        IJobRequisition _jobRequisition { get; set; }
+
+        public JobRequisitionController(IJobRequisition jobRequisition)
+        {
+            _jobRequisition = jobRequisition;
+        }
+
+        //Get Method for Test
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("GetJobRequisition")]
+        public List<JobRequisitionDTO> GetjobRequisition()
+        {
+            return _jobRequisition.GetJobRequisition();
+        }
+
+        //Post Method for Test
+        [HttpPost]
+        [Route("PostCountry")]
+        public ActionResult PostCountry([FromBody] CountryMasterDTO countryDTO)
+        {
+            if (countryDTO == null)
+                return BadRequest();
+
+            var response = _jobRequisition.PostCountry(countryDTO);
+
+            if (response == null)
+                return BadRequest();
+            else
+                return Ok(response);
+        }
+
+    }
+}
