14e4928b4dfc2150eab98e022f275f8480f32af8
Vishnu Telukutla (People Tech Group) <v-vitel@microsoft.com>
2019-02-01 11:31:07 +0530

    --workflow get call implemented

---
+++ PTG.ATS.BLL/WorkflowBAL.cs
@@ -0,0 +1,46 @@
+﻿using PTG.ATS.DAL.Models;
+using PTG.ATS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTG.ATS.BLL
+{
+    public class WorkflowBAL
+    {
+        atsdevContext dbContext = new atsdevContext();
+
+        public List<HiringStageMasterDTO> GetHiringStages()
+        {
+
+            return dbContext.HiringStageMaster.Select(stage => new HiringStageMasterDTO
+            {
+                HiringStageId = stage.HiringStageId,
+                HiringStageName = stage.HiringStageName,
+                Description = stage.Description,
+                IsLocked = stage.IsLocked,
+                IsActive = stage.IsActive,
+                IsDeleted = stage.IsDeleted,
+                CreatedBy = stage.CreatedBy,
+                CreatedDate = stage.CreatedDate,
+                ModifiedBy = stage.ModifiedBy,
+                ModifiedDate = stage.ModifiedDate,
+                HiringStagesWorkflowMaster = dbContext.HiringStagesWorkflowMaster.Where(x => x.HiringStageId == stage.HiringStageId).Select(innerstage => new HiringStagesWorkflowMasterDTO
+                {
+                    HiringStagesWorkflowId = innerstage.HiringStagesWorkflowId,
+                    HiringStagesWorkflowName = innerstage.HiringStagesWorkflowName,
+                    Description = innerstage.Description,
+                    HiringStageId = innerstage.HiringStageId,
+                    IsActive = innerstage.IsActive,
+                    IsDeleted = innerstage.IsDeleted,
+                    CreatedBy = innerstage.CreatedBy,
+                    CreatedDate = innerstage.CreatedDate,
+                    ModifiedBy = innerstage.ModifiedBy,
+                    ModifiedDate = innerstage.ModifiedDate
+                }).ToList()
+            }).ToList();
+
+        }
+    }
+}
---
+++ PTG.ATS.Services/Controllers/WorkflowController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using PTG.ATS.Entities;
+//using Claims.BE.BO;
+//using Claims.BE.DTO;
+using PTG.ATS.Infra;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PTG.ATS.BLL;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PTG.ATS.Services.Controllers
+{
+    //[Authorize]
+    [Produces("application/json")]
+    [Route("api/Workflow")]
+    public class WorkflowController : Controller
+    {
+
+        //Get Method for Test
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("Hiringstage")]
+        public List<HiringStageMasterDTO> GetHiringStageDetails()
+        {
+            WorkflowBAL workflow = new WorkflowBAL();
+            var templist=workflow.GetHiringStages();
+            return templist;
+        }
+
+      
+
+    }
+}
