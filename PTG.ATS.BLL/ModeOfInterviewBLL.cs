679aced1af38e7e4066d82c483d4ea1ca6b9d3a5
Abdul Razak <abdulrazak.feroz786@gmail.com>
2019-02-04 10:38:28 +0530

    Feroz-Location & Mode of Interview

---
+++ PTG.ATS.BLL/ModeOfInterviewBLL.cs
@@ -0,0 +1,30 @@
+﻿using PTG.ATS.DAL.Models;
+using PTG.ATS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PTG.ATS.BLL
+{
+    public class ModeOfInterviewDetails
+    {
+        atsdevContext dbContext = new atsdevContext();
+        public List<ModeOfInterviewMasterDTO> GetModeOfInterviewDetails()
+        {
+            return dbContext.ModeOfInterviewMaster.Select(x => new ModeOfInterviewMasterDTO
+            {
+                ModeOfInterviewId = x.ModeOfInterviewId,
+                ModeOfInterviewName = x.ModeOfInterviewName
+            }).ToList();
+        }
+        public List<ModeOfInterviewMasterDTO> ModeOfInterviewMasterDTO(string modeOfInterview)
+        {
+            return dbContext.ModeOfInterviewMaster.Where(x => x.ModeOfInterviewName == modeOfInterview).Select(x => new ModeOfInterviewMasterDTO
+            {
+                ModeOfInterviewName = x.ModeOfInterviewName
+            }).ToList();
+        }
+
+    }
+}
