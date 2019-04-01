921b85ee9a4f28ad11b807944ec33c16514499f9
Vasudev <vasudev.baggi@ptgindia.com>
2019-04-01 15:08:14 +0530

    InterviewPages

---
+++ PTG.ATS.BLL/InterviewDetails.cs
@@ -0,0 +1,23 @@
+﻿//using PTG.ATS.DAL.Models;
+//using PTG.ATS.Entities;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace PTG.ATS.BLL
+//{
+//    public class InterviewDetails
+//    {
+//        atsdevContext dbContext = new atsdevContext();
+
+//        public List<InterviewDTO> GetInterviewDetailsDetails()
+//        {
+//            return dbContext.Candidate.Select(x => new InterviewDTO
+//            {
+//                CandidateFirstName = x.CandidateFirstName,
+//                CandidateLastName = x.CandidateLastName,
+//                CandidateMiddleName = x.CandidateMiddleName,
+//                CandidateId = x.CandidateId
+//            }).ToList();
+//        }
+//    }
+//}
---
+++ PTG.ATS.Entities/DTO/InterviewDTO.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//namespace PTG.ATS.Entities
+//{
+//    public partial class InterviewDTO
+//    {
+//        public int CandidateId { get; set; }
+//        public string CandidateFirstName { get; set; }
+//        public string CandidateMiddleName { get; set; }
+//        public string CandidateLastName { get; set; }
+//        public int Position { get; set; }
+//        public int Status { get; set; }
+//        public int Time { get; set; }
+//        public string Interviewstage { get; set; }
+//        public string ModeofInterview { get; set; }
+//        public string Comments { get; set; }
+//        public Boolean Followup { get; set; }
+
+
+//    }
+//}
