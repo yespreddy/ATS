e97cf9d49e41ff4eb1d0286293711546d4487cc9
Gunjan Kansara <gunjan.kansara@ptgindia.com>
2019-04-09 15:43:35 +0530

    --GetHRQuestions and GetOtherQuestions

---
+++ PTG.ATS.BLL/IPriliminaryQuestionary.cs
@@ -0,0 +1,11 @@
+﻿using PTG.ATS.Entities;
+using System.Collections.Generic;
+
+namespace PTG.ATS.BLL
+{
+    public interface IPriliminaryQuestionary
+    {
+        List<HrquestionMasterDTO> GetHRQuestions();
+        List<OtherQuestionMasterDTO> GetOtherQuestions();
+    }
+}
---
+++ PTG.ATS.Entities/DTO/OtherQuestionMasterDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTG.ATS.Entities
+{
+    public class OtherQuestionMasterDTO: IBaseEntity
+    {
+        public int OtherQuestionId { get; set; }
+        public string OtherQuestionText { get; set; }
+        public string Description { get; set; }
+        public bool? IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
